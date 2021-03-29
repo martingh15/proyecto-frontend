7cc037732c82daa257426c36be2138b637626fa1
Martín Ghiotti <martin.ghiotti@innovavista.net>
2021-03-28 21:49:13 -0300

    Listado de usuarios

--- src/components/secciones/Usuarios/Listado.js
+++ src/components/secciones/Usuarios/Listado.js
@@ -5,7 +5,10 @@ import {connect} from "react-redux";
 //Actions
 import {resetUsuarios, fetchUsuarios} from "../../../actions/UsuarioActions";
 
-//Boostrap
+//Constants
+import * as rutas from '../../../constants/rutas.js';
+
+//Components
 import Loader from "../../elementos/Loader";
 
 //CSS
@@ -13,17 +16,40 @@ import "../../../assets/css/Usuarios/Listado.css";
 
 //Librerias
 import history from "../../../history";
+import Swal from 'sweetalert2';
+
+//Images
+import lapiz from "../../../assets/icon/pencil.png";
+import tacho from "../../../assets/icon/delete.png";
 
 class Listado extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            noHayUsuarios: false,
+            buscando: true
         }
     }
 
     componentDidMount() {
         this.props.resetUsuarios();
         this.props.fetchUsuarios();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        let allIds      = this.props.usuarios.allIds;
+        let usuarios    = this.props.usuarios.byId;
+        let preUsuarios = prevProps.usuarios.byId;
+        if (preUsuarios.isFetching && !usuarios.isFetching && allIds.length === 0) {
+            this.setState({
+                noHayUsuarios: true,
+            })
+        }
+        if (preUsuarios.isFetching && !usuarios.isFetching) {
+            this.setState({
+                buscando: false,
+            })
+        }
     }
 
     getRolesUsuario(usuario) {
@@ -40,26 +66,77 @@ class Listado extends React.Component {
         return roles;
     }
 
+    modalBorrar(usuario) {
+        Swal.fire({
+            title: `Está seguro de borrar el usuario '${usuario.nombre}'`,
+            icon: 'warning',
+            showCloseButton: true,
+            showCancelButton: true,
+            focusConfirm: false,
+            confirmButtonText: 'Aceptar',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                //Hacer operación borrar usuario
+            }
+        })
+    }
+
+    getOperacionesUsuario(usuario) {
+        let id = usuario.id;
+        let rutaEditar = `${rutas.GESTION_USUARIOS}/editar/${id}`;
+        return (
+            <div>
+                <a href={rutaEditar} title="Editar "
+                   className="operacion">
+                    <img src={lapiz} className="icono-operacion" alt="Editar usuario"/>
+                    Editar
+                </a>
+                <p onClick={() => this.modalBorrar(usuario)} title="Borrar"
+                   className="operacion">
+                    <img src={tacho} className="icono-operacion" alt="Borrar usuario"/>
+                    Borrar
+                </p>
+            </div>
+        );
+    }
+
     render() {
-        const Usuarios = [];
+        const { noHayUsuarios, buscando } = this.state;
+        let Usuarios = [];
+        if (noHayUsuarios) {
+            Usuarios =
+                <tr className="text-center">
+                    <td colSpan="5">No hay usuarios cargados</td>
+                </tr>;
+        }
         this.props.usuarios.allIds.map(idUsuario => {
             let usuario = this.props.usuarios.byId.usuarios[idUsuario];
             if (usuario && usuario.id) {
-                let roles = this.getRolesUsuario(usuario);
+                let roles       = this.getRolesUsuario(usuario);
+                let operaciones = this.getOperacionesUsuario(usuario);
                 Usuarios.push(
                     <tr key={usuario.id}>
                         <td>{usuario.nombre}</td>
                         <td>{usuario.email}</td>
                         <td>{usuario.dni}</td>
                         <td>{roles}</td>
+                        <td>{operaciones}</td>
                     </tr>
                 );
             }
         });
+        const Cargando =
+        <tr>
+            <td></td>
+            <td></td>
+            <td><Loader display={true}/></td>
+            <td></td>
+            <td></td>
+        </tr>;
         return (
             <div className="tabla-usuarios">
-                <div className="table-responsive tarjeta-body"
-                     style={{display: Usuarios.length > 0 ? "block" : "none"}}>
+                <div className="table-responsive tarjeta-body">
+                    <h1 className="tabla-usuarios-titulo">Usuarios</h1>
                     <table className="table">
                         <thead>
                         <tr>
@@ -67,10 +144,11 @@ class Listado extends React.Component {
                             <th>Correo</th>
                             <th>Dni</th>
                             <th>Roles</th>
+                            <th>Operaciones</th>
                         </tr>
                         </thead>
                         <tbody>
-                            {Usuarios}
+                            {buscando ? Cargando : Usuarios}
                         </tbody>
                     </table>
                 </div>
